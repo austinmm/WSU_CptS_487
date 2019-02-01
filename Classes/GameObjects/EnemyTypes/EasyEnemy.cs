944285de35bc30cf4744602db36457095340991e
Austin Marino <marino_austin@yahoo.com>
2019-02-01 14:37:45 -0800

    Seperated all classes in GameClasses.cs into seperate files. Created a version 1 for our class diagram structure called ClassDiagram_V1.cd

---
+++ Classes/GameObjects/EnemyTypes/EasyEnemy.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Threading;
+using System.Diagnostics;
+
+namespace GameClasses
+{
+    public class Easy : Enemy
+    {
+        //Easy Enemy Constructor
+        public Easy() : base()
+        {
+            this.path = EnemyPaths.EasyPath;
+        }
+        public override void Update()
+        {
+            base.Update();
+            //Move Enemy to new position in its set path
+        }
+    }
+}
